0d12b4de5382ee89b18c0b088084c666204007e9
Victor Hugo <pavictorhugo@gmail.com>
2021-08-06 19:55:40 -0300

    solve challenge

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -8,12 +8,55 @@
 
 import Foundation
 
+protocol CoinManagerDelegate {
+    func gotLastRate(_ coinManager: CoinManager, rate: Double)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     
+    var delegate: CoinManagerDelegate?
+    
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "86F9F315-9EA5-460A-A167-81F36AAC7CA1"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-
     
+    func getCoinPrice(for currency: String){
+        let url = "\(baseURL)/\(currency)?apikey=\(apiKey)"        
+        performRequest(with: url);
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            let lastPrice = decodedData.rate
+            
+            return lastPrice
+        } catch {
+            return nil
+        }
+    }
+    
+    func performRequest(with urlString: String){
+        if let url = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if(error != nil){
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let rate = self.parseJSON(safeData){
+                        self.delegate?.gotLastRate(self, rate: rate)
+                    }
+                    
+                }
+            }
+            task.resume()
+        }
+    }
+    
+ 
 }
